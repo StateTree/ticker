820949262807384d06f4647815bb5043a1a0774b
Sanjay krishna anbalagan <sanjay.krishna@cengage.com>
2017-12-18 00:01:48 -0600

    # Conflicts: #	demo/demo.js #	lib/ticker.js #	package.json #	src/demo/index.js #	src/lib/Manager.js #	src/lib/TickEntry.js

--- src/demo/index.js
+++ src/demo/index.js
@@ -1,19 +1,31 @@
 import Ticker from "./../lib";
 
-function callLaterFunction (){
-    console.log("I am called once per frame");
+function firstFunction (){
+    console.log("first Function");
+}
+
+function secondFunction (){
+	console.log("Second Function");
+}
+
+function thirdFunction (){
+	console.log("Third Function");
+}
+
+function fourthFunction (){
+	console.log("Fourth Function");
 }
 
 function callBackFunction (){
-	console.log("I am callback called after");
+	console.log("I am called once per frame last as callback");
 }
 
-function callLaterLastFunction (){
-	console.log("I cam called once per frame last");
-}
+var ticker1 = new Ticker(window, firstFunction, callBackFunction);
+var ticker2 = new Ticker(window, secondFunction, callBackFunction , 1);
+var ticker3 = new Ticker(window, thirdFunction, callBackFunction , 2);
+var ticker4 = new Ticker(window, fourthFunction, callBackFunction , 3);
 
-var ticker1 = new Ticker(window, callLaterFunction, callBackFunction);
-var ticker2 = new Ticker(window, callLaterLastFunction, callBackFunction);
-
-ticker2.executeLast();
+ticker4.execute();
+ticker3.execute();
+ticker2.execute();
 ticker1.execute();
--- src/lib/TickEntry.js
+++ src/lib/TickEntry.js
@@ -1,16 +1,18 @@
 import manager from './Manager';
 
+// toDo: support both callback and promise
 export default class TickEntry
 {
 	/**
 	 * @param {object} context - The "this" argument for the listener function.
 	 * @param {function} listener.
 	 */
-	constructor(context, listener, callback)
+	constructor(context, listener, callback = null, priority = 0)
 	{
 		this.context = context;
 		this.listener = listener;
 		this.callback = callback;
+		this.priority = priority;
 	}
 
 }
@@ -20,14 +22,16 @@ export default class TickEntry
 TickEntry.prototype.dispose = function(){
 	this.context = null;
 	this.listener = null;
+	this.callback = null;
+	this.priority = null;
 };
 
 TickEntry.prototype.execute = function(){
-	manager.add(this,false);
+	manager.add(this);
 };
 
-TickEntry.prototype.executeLast = function(){
-    manager.add(this,true);
+TickEntry.prototype.getMaxPriority = function(){
+	return manager.getMaxPriority();
 };
 
 
